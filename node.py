0cd90956c2b8193ef0e41776d6da3220a7b8d44d
Fernando Javier Aguilar Canto <pherjev@gmail.com>
2022-05-29 13:27:56 -0500

    Add files via upload

---
+++ node.py
@@ -0,0 +1,13 @@
+class Node:
+        """
+        Clase Nodo: Referente a los nodos. 
+        """
+        def __init__(self,id):
+
+                self.id = id
+                self.x = 0
+                self.y = 0
+                self.gx = 0
+                self.gy = 0
+                self.degree = 0
+                self.neighboors = set()
